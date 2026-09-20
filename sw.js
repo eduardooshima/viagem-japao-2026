cff4e41eb8b7a2d5bc83a7a36ca98491de0665fd
eduardooshima <eduardooshima@gmail.com>
2026-09-20 19:18:19 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,26 @@
-const C='japao2026-v10';const A=['./','./index.html','./data.js','./manifest.json','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='japao2026-v11';
+const A=['./','./index.html','./data.js','./manifest.json','./icon.svg'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(C)
+      .then(cache => cache.addAll(A))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(key => key !== C).map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
