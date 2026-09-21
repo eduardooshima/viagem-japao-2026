329ea78f404327e758238efdda42ec70eb256698
eduardooshima <eduardooshima@gmail.com>
2026-09-21 11:46:31 -0300

    restaura service worker da versao funcional

--- sw.js
+++ sw.js
@@ -1,34 +1,26 @@
-const C='japao2026-v26';
+const C='japao2026-v15';
 const A=['./','./index.html','./data.js','./manifest.json','./icon.svg'];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(C).then(cache => cache.addAll(A)).then(() => self.skipWaiting())
+    caches.open(C)
+      .then(cache => cache.addAll(A))
+      .then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys()
-      .then(keys => Promise.all(keys.filter(key => key !== C).map(key => caches.delete(key))))
+      .then(keys => Promise.all(
+        keys.filter(key => key !== C).map(key => caches.delete(key))
+      ))
       .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
-  const url = new URL(event.request.url);
-  const isAppShell = url.pathname.endsWith('/') || url.pathname.endsWith('/index.html');
-  if (isAppShell) {
-    event.respondWith(
-      fetch(event.request, {cache:'no-store'})
-        .then(response => {
-          const copy = response.clone();
-          caches.open(C).then(cache => cache.put(event.request, copy));
-          return response;
-        })
-        .catch(() => caches.match(event.request))
-    );
-  } else {
-    event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
-  }
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
 });
